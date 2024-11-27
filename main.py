5916d96ebfc859eb239ed2217de2c64ab76c9137
Dominik <47948163+weristdominik@users.noreply.github.com>
2024-11-27 22:38:40 +0100

    YouTube downloader INIT

---
+++ main.py
@@ -0,0 +1,13 @@
+# pip install pytubefix
+from pytubefix import YouTube
+from datetime import datetime
+source_url = input("URL:")
+
+extractYB = YouTube(source_url)
+video_stream = extractYB.streams.get_highest_resolution()
+destination_path = 'videos'
+currtime = datetime.today()
+filename = str(currtime.hour) + str(currtime.minute) + str(currtime.second) + '.mp4'
+video_stream.download(output_path=destination_path, filename=filename)
+video_size_bytes = video_stream.filesize
+video_duration_seconds = extractYB.length
